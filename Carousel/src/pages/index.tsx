4b709dd90452b80b100c6e19aadfcb0b23cbde4f
dohyun <dodohyun0807@gmail.com>
2022-05-25 16:36:55 +0900

    DOCS: main페이지 수정

---
+++ Carousel/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "../components/carousel/carousel";
+import "./index.css";
+
+const Home = () => {
+	return (
+		<div className="MainContainer">
+			<nav>Dohyun's Carousel</nav>
+			<Carousel />
+		</div>
+	);
+};
+
+export default Home;
