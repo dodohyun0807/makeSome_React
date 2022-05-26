f7321cb61da6e385b8a1fe0b2c0d0288192df275
dohyun <dodohyun0807@gmail.com>
2022-05-26 21:04:44 +0900

    DOCS: Carousel CSS 세부 디자인 변경 및 이벤트 적용 완료

--- Carousel/src/components/carousel/carousel.jsx
+++ Carousel/src/components/carousel/carousel.jsx
@@ -9,42 +9,54 @@ const Carousel = () => {
 	const [currentSlide, setCurrentSlide] = useState(0);
 	const [images, setImages] = useState([
 		"img/1.jpg",
-		"img/1.jpg",
-		"img/1.jpg",
-		"img/1.jpg",
-		"img/1.jpg",
-		"img/1.jpg",
-		"img/1.jpg",
-		"img/1.jpg",
+		"img/2.jpg",
+		"img/3.jpg",
+		"img/4.jpg",
+		"img/5.jpg",
+		"img/6.jpg",
+		"img/7.jpg",
+		"img/2.jpg",
 	]);
 
 	const moveRight = () => {
-		setCurrentSlide(currentSlide + 85);
+		if (images.length * 100 - 100 > currentSlide + 100) {
+			setCurrentSlide(currentSlide + 100);
+		}
+		else {
+			setCurrentSlide(0);
+		}
 	};
 	const moveLeft = () => {
-		setCurrentSlide(currentSlide - 85);
+		if (currentSlide > 1) {
+			setCurrentSlide(currentSlide - 100);
+		}
+		else {
+			setCurrentSlide(images.length * 100 - 100);
+		}
 	};
 
 	useEffect(() => {
 		slideRef.current.style.transition = "all 0.5s ease-in-out";
-		slideRef.current.style.transform = `translateX(-${currentSlide}vw)`;
+		slideRef.current.style.transform = `translateX(-${currentSlide}%)`;
 	}, [currentSlide]);
 
 	return (
-		<div className="CarouselContainer">
-			<nav className="TitleContainer">Images Carousel</nav>
-			<div className="ContentContainer">
-				<button type="button" onClick={moveLeft}>
+		<div className="carouselContainer">
+			<nav className="titleContainer">Images Carousel</nav>
+			<div className="contentContainer">
+				<button type="button" onClick={moveLeft} className="slideBtn">
 					<FontAwesomeIcon icon={faAngleLeft} />
 				</button>
-				<div className="ImageContainer">
-					<div className="SlideContainer" ref={slideRef}>
+				<div className="slideBox">
+					<ul className="slideList" ref={slideRef} >
 						{images.map((item, idx) => (
-							<img key={idx} src={item} className="ImageItem" />
+							<li className="slideItem">
+								<img key={idx} src={item} className="image" />
+							</li>
 						))}
-					</div>
+					</ul>
 				</div>
-				<button type="button" onClick={moveRight}>
+				<button type="button" onClick={moveRight} className="slideBtn">
 					<FontAwesomeIcon icon={faAngleRight} />
 				</button>
 			</div>
