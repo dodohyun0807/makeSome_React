754ee83669c7dfc597a76d14d5e0b7c620bf9f19
dohyun <dodohyun0807@gmail.com>
2022-05-30 01:54:00 +0900

    DOCS: craco제거, alias픽스 완료

---
+++ Modal/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,8 @@
+import { FunctionComponent } from 'react';
+import Style from './Navbar.module.css';
+
+const Navbar: FunctionComponent = () => {
+  return <div className={Style.test}>hello</div>;
+};
+
+export default Navbar;
