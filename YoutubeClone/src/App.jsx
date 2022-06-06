76788a51b5ba0c1d5eba77eff07fe61c4a7f2370
dohyun <dodohyun0807@gmail.com>
2022-06-06 19:56:27 +0900

    DOCS: youtubeAPI 데이터 받아오기 완료

--- YoutubeClone/src/App.jsx
+++ YoutubeClone/src/App.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-	return <div className="App">hello</div>;
+	const [videos, setvideos] = useState([])
+
+	useEffect(() => {
+		const requestOptions = {
+			method: 'GET',
+			redirect: 'follow',
+		}
+
+		fetch(
+			'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDQXYg0-yzGfO-uRF3j3yv0uJAcKFfOkwQ',
+			requestOptions
+		)
+			.then(res => res.json())
+			.then(result => setvideos(result.items))
+			.catch(err => console.log('error', err))
+	}, [])
+
+	return (
+		<>
+			<h1>hello!</h1>
+			<VideoList video={videos} />
+		</>
+	)
 }
 
 export default App;
---
+++ YoutubeClone/src/components/video_item/video_item.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VideoItem = (props) => {
+  console.log(props)
+  return (
+    <li>
+      {props.video.snippet.title}
+    </li>
+  );
+};
+
+export default VideoItem;
