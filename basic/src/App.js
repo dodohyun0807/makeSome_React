e1af464484da09868da93505e61543d9579c0b56
dohyun <dodohyun0807@gmail.com>
2023-02-01 21:56:41 +0900

    새로운 회원일경우 new태그 삽입(map/foreach 차이, span과 absolute)

--- basic/src/App.js
+++ basic/src/App.js
@@ -1,12 +1,46 @@
-import {Profile} from './components/Profile'
-import './App.css';
+import { Profile } from "./components/Profile";
+import "./App.css";
 
 function App() {
+  const people = [
+    {
+      pk: 1,
+      url: "https://cdn.mhnse.com/news/photo/202211/156007_150435_2723.jpg",
+      des: "카리나",
+      name: "카리나",
+      job: "가수",
+      isNew: true,
+    },
+    {
+      pk: 2,
+      url: "https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/04/29/0adb0b65-4976-4fdd-96bb-c0be1c558637.jpg",
+      des: "이주빈",
+      name: "이주빈",
+      job: "배우",
+      isNew: false,
+    },
+    {
+      pk: 3,
+      url: "https://img.hankyung.com/photo/202103/03.21325653.1.jpg",
+      des: "설인아",
+      name: "설인아",
+      job: "배우",
+      isNew: true,
+    },
+  ];
+
   return (
     <div className="App">
-      < Profile url='https://cdn.mhnse.com/news/photo/202211/156007_150435_2723.jpg' des='카리나' name='카리나' job='가수'/>
-      < Profile url='https://talkimg.imbc.com/TVianUpload/tvian/TViews/image/2022/04/29/0adb0b65-4976-4fdd-96bb-c0be1c558637.jpg' des='이주빈' name='이주빈' job='배우'/>
-      < Profile url='https://img.hankyung.com/photo/202103/03.21325653.1.jpg' des='설인아' name='설인아' job='배우'/>
+      {people.map((id) => (
+        <Profile
+          key={id.pk}
+          url={id.url}
+          des={id.des}
+          name={id.name}
+          job={id.job}
+          isNew={id.isNew}
+        />
+      ))}
     </div>
   );
 }
--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
+import React from "react";
 
-export const Profile = ({url, des, name, job}) => {
-    console.log(url)
-    return (
-        <div className='profile'>
-            {url === undefined
-            ? <h1>none!</h1>
-            : <>
-                <img className='photo' 
-                src={url}
-                alt={des}
-                />
-                <h1>{name}</h1>
-                <p>{job}</p>
-                </>
-            }
-    
-            </div>
-    )
+export const Profile = ({ url, des, name, job, isNew }) => {
+  console.log(url);
+  return (
+    <div className="profile">
+      {isNew && <span className="label_new">new!</span>}
+      {url === undefined ? (
+        <h1>none!</h1>
+      ) : (
+        <>
+          <img className="photo" src={url} alt={des} />
+          <h1>{name}</h1>
+          <p>{job}</p>
+        </>
+      )}
+    </div>
+  );
 };
