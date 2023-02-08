0f00cc34bb99b3ea81064cc94cc1a7ca87611d17
dohyun <dodohyun0807@gmail.com>
2023-02-08 20:49:55 +0900

    상태관리 immer로 변경

--- mouse-pointer/src/AppMentors.jsx
+++ mouse-pointer/src/AppMentors.jsx
@@ -1,5 +1,5 @@
-import React, { useReducer } from "react";
-import personReducer from "./reducer/person-reducer";
+import React from "react";
+import { useImmer } from "use-immer";
 
 export default function AppMentor() {
   const initialPerson = {
@@ -17,23 +17,31 @@ export default function AppMentor() {
     ],
   };
 
-  const [person, dispatch] = useReducer(personReducer, initialPerson);
+  const [person, updatePerson] = useImmer(initialPerson);
 
   const handleClickChange = () => {
     const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-    dispatch({ type: "updated", prev, current });
+    updatePerson((person) => {
+      const mentor = person.mentors.find((m) => m.name === prev);
+      mentor.name = current;
+    });
   };
 
   const handleClickAdd = () => {
     const name = prompt("추가할 멘토의 이름은?");
     const title = prompt("추가할 멘토의 타이틀은?");
-    dispatch({ type: "added", name, title });
+    updatePerson((person) => {
+      person.mentors.push({ name, title });
+    });
   };
 
   const handleClickRemove = () => {
     const name = prompt("삭제할 멘토의 이름은?");
-    dispatch({ type: "removed", name });
+    updatePerson((person) => {
+      const mentor = person.mentors.findIndex((m) => m.name === name);
+      person.mentors.splice(mentor, 1);
+    });
   };
 
   return (
